a6c0018ac4d61d7a28c70a2cac5eb844ef0e6e97
Matheus <matheusvazti@gmail.com>
2023-02-08 19:43:40 -0300

    Finalizando projeto

--- scripts.js
+++ scripts.js
@@ -7,7 +7,9 @@ function colocarNaTela(dados){
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) + "°c"
     document.querySelector(".descricao").innerHTML = dados.weather[0].description
-    document.querySelector(".iconi").src = ""
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".humidade").innerHTML =  (dados.main.humidity) + "%"
+
 }
 
 async function buscarCidade(city) {
@@ -28,7 +30,7 @@ async function buscarCidade(city) {
 }
 
 function cliqueiNoBotao() {
-    let city = document.querySelector(".input-cidade").value
+    let cidade = document.querySelector(".input-cidade").value
 
-    buscarCidade(city)
+    buscarCidade(cidade)
 }
